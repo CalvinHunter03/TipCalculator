8df67fef0270be89469f76fd83d5d81e85eb0f11
Calvin Hunter <calhunte@ucsc.edu>
2024-10-08 13:59:14 -0700

    Step 2 Complete
    Buttons now will count depending on whats going on.

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,53 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const buttonName = "🥛";
-const button = document.createElement("button");
-button.innerHTML = buttonName;
-app.append(button);
+let milkCounter: number = 0;
+let cookieCounter: number = 0;
+
+//Milk button
+const milkButtonName = "🥛";
+const milkButton = document.createElement("button");
+milkButton.innerHTML = milkButtonName;
+app.append(milkButton);
+
+//cookie button
+const cookieButtonName = "🍪";
+const cookieButton = document.createElement("button");
+cookieButton.innerHTML = cookieButtonName;
+app.append(cookieButton);
+
+//Milk text
+const milkDiv = document.createElement("div");
+const milkDivText = document.createTextNode("0 glasses of milk");
+milkDiv.appendChild(milkDivText);
+app.append(milkDiv);
+
+//cookie text
+const cookieDiv = document.createElement("div");
+const cookieDivText = document.createTextNode("0 cookies");
+cookieDiv.appendChild(cookieDivText);
+app.append(cookieDiv);
+
+//milk button counter
+milkButton.addEventListener("click", () => {
+    milkCounter += 1;
+    if(milkCounter === 1){
+        milkDivText.textContent = `${milkCounter} glass of milk`;
+    }
+    else{
+    milkDivText.textContent = `${milkCounter} glasses of milk`;
+    }
+});
+
+//cookie button counter
+cookieButton.addEventListener("click", () => {
+    if(milkCounter != 0){
+        cookieCounter += 1;
+        if (cookieCounter === 1){
+            cookieDivText.textContent = `${cookieCounter} cookie`;
+        }
+        else{
+        cookieDivText.textContent = `${cookieCounter} cookies`
+        }
+    }
+});
