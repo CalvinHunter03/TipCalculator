e4d1ee8d26c48f154c2b915884234d08ddca401d
Calvin Hunter <calhunte@ucsc.edu>
2024-12-01 15:37:00 -0800

    added some things, no functionality yet

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,29 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Tip Calculator";
 document.title = gameName;
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
+const prompt = document.createElement("h2");
+prompt.innerHTML = `Enter total amount`;
+app.append(prompt);
+
+const totalInput = document.createElement("input");
+totalInput.type = "number";
+app.append(totalInput);
+
+//spacers
+app.append(document.createElement("div"));
+
+const calculateButton = document.createElement("button");
+calculateButton.innerHTML = "Calculate";
+app.append(calculateButton);
+
+const percentageSlider = document.createElement("input");
+percentageSlider.type = "range";
+percentageSlider.min = "0";
+percentageSlider.max = "100";
+app.append(percentageSlider);
+
+calculateButton.addEventListener("click", () => {
+  if (totalInput.value === ``) {
+    alert("Enter your total");
+  }
+});
